ce54a0c393caa48616c6556bd7229c7a011ea169
Andrew Ibarra <ibarra7751@gmail.com>
2020-02-24 23:36:42 -0500

    Changes to SavingsAccountTest

--- Ibarra_problem2/src/SavingsAccountTest.java
+++ Ibarra_problem2/src/SavingsAccountTest.java
@@ -6,17 +6,17 @@ public class SavingsAccountTest
 		SavingsAccount saver1 = new SavingsAccount();
 		SavingsAccount saver2 = new SavingsAccount();
 		
-		System.out.println("     4% Interest");
+		System.out.println("Saver 1    4% Interest");
 		saver1.annualInterestRate = .04;
 		saver1.calculateMonthlyInterest(2000.00);
-
+		System.out.println("Saver 2    4% Interest");
 		saver2.annualInterestRate = .04;
 		saver2.calculateMonthlyInterest(3000.00);
 
-		System.out.println("     5% Interest");
+		System.out.println("Saver 1     5% Interest");
 		saver1.annualInterestRate = .05;
 		saver1.calculateMonthlyInterest(2000.00);
-
+		System.out.println("Saver 2     5% Interest");
 		saver2.annualInterestRate = .05;
 		saver2.calculateMonthlyInterest(3000.00);
 
